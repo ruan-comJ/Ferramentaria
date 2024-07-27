a502231f71971c3ed011bf567d1d8e33145c5ae9
Juan Alejandro <163759712+ruan-comJ@users.noreply.github.com>
2024-07-27 19:01:36 -0300

    Adicionado opção de adicionar mais itens ao termo
    Adicionado a opção de voltar ao menu inicial ao acessar as opções do funcionário

--- Ferramentaria/Principal.cs
+++ Ferramentaria/Principal.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Runtime.InteropServices;
 using System.Runtime.Intrinsics.X86;
 using System.Threading.Channels;
 
@@ -14,10 +15,11 @@ namespace Ferramentaria
             int matricula; //Variável global
 
             int opcao = 0;
-
+            int index = 0;
             int indiceTermo;
             int indiceTermoAdd = 0;
 
+            //----------------------------------------------- M E N U  P R I N C I P A L ----------------------------------------------------//
             while (opcao != 3)
             {
                 Console.WriteLine("MENU FERRAMENTARIA\n");
@@ -52,7 +54,11 @@ namespace Ferramentaria
                                 encontrado = true;
                                 indiceTermo = i;
                                 Console.WriteLine("\nOpções: ");
-                                Console.WriteLine("\n1 - Mostrar termo.\n" + "2 - Adicionar item ao termo.\n" + "3 - Remover item do termo");
+                                Console.WriteLine("" +
+                                    "\n1 - Mostrar termo.\n" 
+                                    + "2 - Adicionar item ao termo.\n" 
+                                    + "3 - Remover item do termo\n" 
+                                    + "4 - Voltar ao menu inicial");
                                 Console.Write("\nEscolha um opção: ");
                                 int opcaoFuncionario = int.Parse(Console.ReadLine());
                                 Console.Clear();
@@ -61,24 +67,33 @@ namespace Ferramentaria
                                 {
                                     case 1:
                                         for (indiceTermo = 0; indiceTermo < funcionarios[i].Termo.Length && funcionarios[i].Termo[indiceTermo] != null; indiceTermo++)
-                                        Console.WriteLine(indiceTermo + " - " + funcionarios[i].Termo[indiceTermo] + "\n");
+                                            Console.WriteLine(indiceTermo + " - " + funcionarios[i].Termo[indiceTermo] + "\n");
                                         break;
 
                                     case 2:
-                                        Console.Write("Insira o nome do item a ser adicionado ao termo do funcionário: ");
-                                        funcionarios[i].Termo[indiceTermoAdd] = Console.ReadLine();
-                                        Console.Clear();
-                                        Console.WriteLine(funcionarios[i].Termo[indiceTermoAdd] + " adicionado ao termo com sucesso!\n");
-                                        indiceTermoAdd++;
+                                        char maisItens = 's';
+                                        while (maisItens == 's')
+                                        {
+                                            Console.Write("Insira o nome do item a ser adicionado ao termo do funcionário: ");
+                                            funcionarios[i].Termo[indiceTermoAdd] = Console.ReadLine();
+                                            Console.Clear();
+                                            Console.WriteLine(funcionarios[i].Termo[indiceTermoAdd] + " adicionado ao termo com sucesso!\n");
+                                            indiceTermoAdd++;
+                                            Console.Write("Deseja adicionar mais itens ao termo (s/n) ? ");
+                                            maisItens = char.Parse(Console.ReadLine());
+                                        }
                                         break;
 
                                     case 3:
                                         for (indiceTermo = 0; indiceTermo < funcionarios[i].Termo.Length && funcionarios[i].Termo[indiceTermo] != null; indiceTermo++)
                                             Console.WriteLine(indiceTermo + " - " + funcionarios[i].Termo[indiceTermo] + "\n");
-                                        Console.Write("Escolha o númerro correspondente ao item para remove-lo do termo: ");
+                                        Console.Write("Escolha o número correspondente ao item para remove-lo do termo: ");
                                         int numRemover = int.Parse(Console.ReadLine());
                                         funcionarios[i].Termo[numRemover] = null;
                                         Console.WriteLine("Item removido do termo com sucesso!");
+                                        break;
+
+                                        case 4:
                                         break;
                                 }
 
@@ -99,7 +114,6 @@ namespace Ferramentaria
                         matricula = int.Parse(Console.ReadLine());
                         Console.Write("\nInsira a função do funcionário: ");
                         string funcao = Console.ReadLine();
-                        int index = 0;
                         funcionarios[index] = new Funcionarios(nome, matricula, funcao);
                         Console.WriteLine("\nFuncionário cadastrado com sucesso!\n");
                         index++;
